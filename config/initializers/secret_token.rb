4aed1b8104336f0ba28411e8a3591b01d3de1a52
Juan Wajnerman <jwajnerman@manas.com.ar>
2011-05-13 00:02:24 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sched::Application.config.secret_token = '05e00a13d9b423915c13f6d252c05fa59ca20d21100a4247bd7625b71ffdd884c7405e4df1219a0e56a71e37e5e7308cfc38c6845b91615ecffa4d919e8b3aaf'
