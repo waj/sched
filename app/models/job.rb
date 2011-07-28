84b97c7d275adb1a71c94a09133e9089ffc23dc9
Juan Wajnerman <jwajnerman@manas.com.ar>
2011-07-28 17:46:56 -0300

    Implemented scheduler

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,38 @@
+require 'net/http'
+
 class Job < ActiveRecord::Base
+  MAX_RETRIES = 5
+
+  def self.run_loop
+    Job.where('at <= ?', DateTime.now).find_each do |job|
+      job.run
+    end
+  end
+
+  def run
+    if recurrent?
+      # schedule next cycle
+      self.at = interval.minutes.from_now
+    else
+      self.retries += 1
+      return delete if retries > MAX_RETRIES
+      # schedule next retry
+      self.at = 5.seconds.from_now
+    end
+    save!
+
+    begin
+      Net::HTTP.get URI.parse(url)
+    rescue Exception => ex
+      puts ex.message
+      delete if retries >= MAX_RETRIES
+      return
+    end
+    delete unless recurrent?
+  end
+
+  def recurrent?
+    interval.present? && interval > 0
+  end
+
 end
--- test/unit/job_test.rb
+++ test/unit/job_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "job run loop" do
+    j1 = Job.create! :at => 10.minutes.ago
+    j2 = Job.create! :at => 1.minute.ago
+    j3 = Job.create! :at => 2.minutes.from_now
+
+    j1.expects(:run).once
+    j2.expects(:run).once
+    j3.expects(:run).never
+
+    Job.run_loop
   end
 end
